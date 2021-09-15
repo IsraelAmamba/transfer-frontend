dce2682be7c255feef9116d407fac6814d0292eb
israel amamba <i.amamba@yahoo.com>
2021-09-15 01:55:29 +0100

    final commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,7 @@ import { AppComponent } from './app.component';
 import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 import {CdkStepperModule} from '@angular/cdk/stepper';
 import {NgStepperModule} from 'angular-ng-stepper';
-import { StepOneComponent } from './steps/step-one/step-one.component';
-import { StepTwoComponent } from './steps/step-two/step-two.component';
-import { StepThreeComponent } from './steps/step-three/step-three.component';
 import { LocalToLocalComponent } from './transfer/own-account/local-to-local/local-to-local.component';
-import { StepFourComponent } from './steps/step-four/step-four.component';
 import { LeftnavComponent } from './layout/leftnav/leftnav.component';
 import { TopnavComponent } from './layout/topnav/topnav.component';
 import { FooterComponent } from './layout/footer/footer.component';
@@ -33,11 +29,7 @@ import { AccountService } from './core/services/application/account.service';
 @NgModule({
   declarations: [
     AppComponent,
-    StepOneComponent,
-    StepTwoComponent,
-    StepThreeComponent,
     LocalToLocalComponent,
-    StepFourComponent,
     LeftnavComponent,
     TopnavComponent,
     FooterComponent,
